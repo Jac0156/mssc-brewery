9577cce454fbf84c6855b606733d5672828ca5db
Jack Weby <jac0156@hotmail.com>
2024-09-04 11:40:22 -0400

    Ajouter POST, PUT, DELETE customer

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -6,4 +6,10 @@ import guru.springframework.msscbrewery.web.model.CustomerDto;
 
 public interface CustomerService {
     CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -5,14 +5,36 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+            .id(UUID.randomUUID())
+            .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // TODO Implement real update
+        log.debug("Upating customer...");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        // TODO Implement real update
+        log.debug("Deleting customer...");
     }
    
 }
