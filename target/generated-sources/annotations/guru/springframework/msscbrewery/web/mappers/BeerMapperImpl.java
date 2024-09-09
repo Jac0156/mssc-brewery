0233bb62b543afd83e9122bef0f770c737307d95
Jack Weby <jac0156@hotmail.com>
2024-09-08 20:08:09 -0400

    Setup and using MapStruct

---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,47 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Beer;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-08T20:04:29-0400",
+    comments = "version: 1.6.0, compiler: Eclipse JDT (IDE) 3.39.0.v20240820-0604, environment: Java 17.0.12 (Eclipse Adoptium)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto.BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.beerName( beer.getBeerName() );
+        beerDto.beerStyle( beer.getBeerStyle() );
+        beerDto.id( beer.getId() );
+        beerDto.upc( beer.getUpc() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Beer.BeerBuilder beer = Beer.builder();
+
+        beer.beerName( dto.getBeerName() );
+        beer.beerStyle( dto.getBeerStyle() );
+        beer.id( dto.getId() );
+        beer.upc( dto.getUpc() );
+
+        return beer.build();
+    }
+}
